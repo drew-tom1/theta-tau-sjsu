7f66968ed9d7910abd75e8e76bc47e614b2b609b
walan0405 <walan0405@gmail.com>
2025-02-21 13:35:50 -0800

    Integrated TS on FAQ page

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -4,8 +4,67 @@ import { faMinus, faP } from "@fortawesome/free-solid-svg-icons";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Script from "next/script";
 import Navbar from "../components/Navbar";
+import { useEffect } from "react";
 
 export default function faq() {
+  // FAQ Typescript
+
+  useEffect(() => {
+    const faqContainer = document.querySelector(".faq-content");
+
+    const handleClick = (e: MouseEvent) => {
+      const groupHeader = e.target.closest(".faq-group-header") as HTMLElement;
+
+      if (!groupHeader) return;
+
+      const group = groupHeader.parentElement;
+      const groupBody = group?.querySelector(".faq-group-body");
+      const icon = groupHeader.querySelector("svg");
+
+      // FAQ answer visibility
+      if (groupBody) {
+        groupBody.classList.toggle("open");
+      }
+
+      // Close other OPEN faq Body
+      const otherGroups = faqContainer?.querySelectorAll(".faq-group");
+
+      otherGroups?.forEach((otherGroups) => {
+        if (otherGroups !== group) {
+          const otherGroupBody = otherGroups.querySelector(".faq-group-body");
+          const otherIcon = otherGroups.querySelector(".faq-group-header svg");
+
+          otherGroupBody?.classList.remove("open");
+          otherIcon?.classList.remove('fa-minus');
+          otherIcon?.classList.add('fa-plus');
+        }
+      });
+
+      // Icon Toggle (its high key low key not workinggggg ;-; ;-; ;-; ;-; ;-; 
+
+      if (icon) {
+        if (icon.classList.contains("fa-plus")) {
+          icon.classList.remove("fa-plus");
+          icon.classList.add("fa-minus");
+        } else {
+          icon.classList.remove("fa-minus");
+          icon.classList.add("fa-plus");
+        }
+      }
+      
+    };
+
+    if (faqContainer) {
+      faqContainer.addEventListener("click", handleClick);
+    }
+
+    return () => {
+      if (faqContainer) {
+        faqContainer.removeEventListener("click", handleClick);
+      }
+    };
+  }, []);
+
   return (
     <>
       {/* Head Section */}
@@ -21,41 +80,43 @@ export default function faq() {
       <section className="faq">
         <div className="container-small">
           <h3 className="text-xl light-color">Frequently Asked Questions</h3>
+          
 
-          <ul className="faq-menu">
-            <li className="active">All</li>
-            <li>Getting Started</li>
-            <li>Pricing</li>
-          </ul>
+          {/* FAQ Menu Not 100% if we need to implement tbh */}
+          {/* <ul className="faq-menu">
+            <li className="active">Rush</li>
+            <li>Events</li>
+            <li>Time</li>
+          </ul> */}
 
           <div className="faq-content">
             <div className="faq-group">
               <div className="faq-group-header text-m">
-                <h4>Question</h4>
-                <FontAwesomeIcon className="fa fa-minus" icon={faMinus} />
+                <h4>How much are dues?</h4>
+                <FontAwesomeIcon icon={faMinus} className="fa fa-minus" />
               </div>
-              <div className="faq-group-body open text-s">
-                <h4>Anwser</h4>
-              </div>
-            </div>
-
-            <div className="faq-group">
-              <div className="faq-group-header text-m">
-                <h4>Question</h4>
-                <FontAwesomeIcon className="fa fa-plus" icon={faPlus} />
-              </div>
-              <div className="faq-group-body text-s">
-                <h4>Anwser</h4>
+              <div className="faq-group-body open text-s text-thin">
+                <p>$200</p>
               </div>
             </div>
 
             <div className="faq-group">
               <div className="faq-group-header text-m">
-                <h4>Question</h4>
-                <FontAwesomeIcon className="fa fa-plus" icon={faPlus} />
+                <h4>Is there any hazing?</h4>
+                <FontAwesomeIcon icon={faMinus} className="fa fa-minus" />
               </div>
-              <div className="faq-group-body text-s">
-                <h4>Anwser</h4>
+              <div className="faq-group-body text-s text-thin">
+                <p>No.</p>
+              </div>
+            </div>
+
+            <div className="faq-group">
+              <div className="faq-group-header text-m">
+                <h4>What are the benifits?</h4>
+                <FontAwesomeIcon icon={faMinus} className="fa fa-minus" />
+              </div>
+              <div className="faq-group-body text-s text-thin">
+                <p>You can get to meet friends and people.</p>
               </div>
             </div>
           </div>
